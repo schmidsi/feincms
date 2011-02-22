a3748951a4bd6fc4e6c24883c6f4bc8685c3b5cf
Simon Schmid <ssc@feinheit.ch>
2011-02-22 12:23:47 +0100

    small bugfix

--- feincms/module/medialibrary/models.py
+++ feincms/module/medialibrary/models.py
@@ -86,7 +86,7 @@ class MediaFileBase(Base, TranslatedObjectMixin):
     fs = default_storage(location=settings.FEINCMS_MEDIALIBRARY_ROOT,
                            base_url=settings.FEINCMS_MEDIALIBRARY_URL)
 
-    file = models.FileField(_('file'), max_length=255, upload_to=settings.FEINCMS_MEDIALIBRARY_UPLOAD_TO, storage=get_default_storage())
+    file = models.FileField(_('file'), max_length=255, upload_to=settings.FEINCMS_MEDIALIBRARY_UPLOAD_TO, storage=fs)
     type = models.CharField(_('file type'), max_length=12, editable=False, choices=())
     created = models.DateTimeField(_('created'), editable=False, default=datetime.now)
     copyright = models.CharField(_('copyright'), max_length=200, blank=True)
